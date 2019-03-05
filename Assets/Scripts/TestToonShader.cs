3a7f1137ad6355d6023575ace87bd108dacee770
Rhiannan Berry <rhiannanb97@gmail.com>
2019-03-05 16:28:41 -0500

    Redoing all dis so i can have shadergraph B)

---
+++ Assets/Scripts/TestToonShader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestToonShader : MonoBehaviour
+{
+
+    public Transform l;
+    SkinnedMeshRenderer r;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<SkinnedMeshRenderer>();
+        r.sharedMaterials[0].SetVector("Vector3_B25F064F", l.position);
+        r.sharedMaterials[1].SetVector("Vector3_B25F064F", l.position);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        r.sharedMaterials[0].SetVector("Vector3_B25F064F", l.position);
+        r.sharedMaterials[1].SetVector("Vector3_B25F064F", l.position);
+
+    }
+}
